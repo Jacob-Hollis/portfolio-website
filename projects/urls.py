b42bb27f0d2da233e524a1ca347472445a1634a2
Jacob Hollis <jbhollis@pm.me>
2023-04-26 14:45:54 -0500

    Deleted project details template
    Not needed, not going with a detailed card

--- projects/urls.py
+++ projects/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.project_index, name="projects"),
-    path("<int:pk>/", views.project_detail, name="project_detail"),
+    path("", views.project_index, name="projects")
 ]
--- projects/views.py
+++ projects/views.py
@@ -8,10 +8,3 @@ def project_index(request):
         'projects': projects
     }
     return render(request, 'project_index.html', context)
-
-def project_detail(request, pk):
-    project = Project.objects.get(pk=pk)
-    context = {
-        'project': project
-    }
-    return render(request, 'project_detail.html', context)
