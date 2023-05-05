d8545c13f9401dbc5dc0ae7080d7116b92421aa9
Jacob Hollis <jbhollis@pm.me>
2023-05-05 16:14:19 -0500

    close to finishing static

--- website/website/settings.py
+++ website/website/settings.py
@@ -128,6 +128,10 @@ STATIC_URL = '/static/static/'
 
 STATIC_ROOT = '/vol/web/static'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder'
